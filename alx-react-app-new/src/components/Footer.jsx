b65f352471c8f3ed2dc5760bba1b16658758b97d
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 01:11:48 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,8 +1,9 @@
+// src/components/Footer.jsx
 import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px', borderRadius: '8px' }}>
       <p>© 2023 City Lovers</p>
     </footer>
   );
