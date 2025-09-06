b791f4cf84cac87c71dceabd30a7541ac410a56d
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 01:17:05 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       {/* User Profiles */}
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <UserProfile name="Bob" age={30} bio="Enjoys cooking and traveling" />
+
+      {/* Counter */}
+      <Counter />
 
       <Footer />
     </div>
