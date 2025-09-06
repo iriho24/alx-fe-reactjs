429429c94c5c8d55a856966871ac7a165a83a5f2
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 01:55:42 +0200

    Update UserProfile.jsx

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -6,8 +6,8 @@ function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '20px', borderRadius: '8px' }}>
-      <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+    <div>
+      <h2>{userData.name}</h2>
       <p>Age: {userData.age}</p>
       <p>Bio: {userData.bio}</p>
     </div>
