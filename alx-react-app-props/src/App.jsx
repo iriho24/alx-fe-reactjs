181213c7e01fe2a2c4f2c1269b5597abdbc575b3
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 01:55:09 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
 import React from 'react';
-import UserProfile from './UserProfile';
 import UserContext from './UserContext';
+import UserProfile from './UserProfile';
 
 function App() {
   const userData = {
     name: "Jane Doe",
-    age: 28,
+    age: 25,
     bio: "Loves hiking and photography"
   };
 
