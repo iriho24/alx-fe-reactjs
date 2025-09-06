3b6f7ba565be86f491a298171123c9a2b87920de
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 01:50:31 +0200

    Update and rename ProfilePage.jsx to UserProfile.jsx

---
+++ alx-react-app-props/src/UserProfile.jsx
@@ -0,0 +1,17 @@
+// src/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '15px', margin: '20px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
