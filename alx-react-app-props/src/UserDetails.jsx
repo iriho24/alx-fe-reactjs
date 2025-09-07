ec1d15a9fb67b4984a9fa989686ab963d32bb373
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-07 02:00:59 +0200

    Update UserDetails.jsx

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,11 +1,12 @@
-import { useContext } from 'react';
+// src/UserDetails.jsx
+import React, { useContext } from 'react';
 import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
-    <div>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '5px' }}>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
