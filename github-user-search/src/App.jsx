bcbebd84c522984afe9b91815a57437af35023fc
IRIHO GERMAIN <91696295+iriho24@users.noreply.github.com>
2025-09-19 16:39:24 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UserList from "./components/UserList";
+import React from "react";
+import Search from "./components/Search";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
   return (
-    <div style={{ padding: "1rem" }}>
+    <div className="App">
       <h1>GitHub User Search</h1>
-      <SearchBar setUsers={setUsers} />
-      <UserList users={users} />
+      <Search />
     </div>
   );
 }
