70e41158e9adcd4af0c96ea578b81068eb70edc2
Germain Iriho <logangermain4@gmail.com>
2025-10-05 22:45:18 +0200

    Add recipe-sharing-platform React project with Tailwind CSS setup

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: { extend: {} },
   plugins: [],
 }
